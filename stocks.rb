6ca38ac55000b7a0fb81f357d0a5c5df6dd01b1c
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-04-17 16:51:45 -0700

    created stocks func

---
+++ stocks.rb
@@ -0,0 +1,16 @@
+def stock_picker([17,3,6,9,15,8,6,1,10])
+    #iterate over array
+end
+
+
+
+
+
+
+
+=begin
+you just need to find a way to iterate through an array of values,
+and pick two values in sequential order that would yield
+the greatest difference when b is subtracted from a
+i.e., (A - B = difference)
+=end
