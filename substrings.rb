5e68288fa9d405905d0e20eb2360382087cd12b0
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-04-21 14:01:15 -0700

    removed extraneous line

--- substrings.rb
+++ substrings.rb
@@ -3,8 +3,7 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 def substrings(str, dictionary)
     output = {}
     dictionary.each do |substring| #iterates over dictionary
-      str.downcase.scan(substring) #checks str against each value in dictionary
-      count = str.downcase.scan(substring).count #gather the count
+      count = str.downcase.scan(substring).count #checks str against each value in dictionary and gathers the count
       output[substring] = count if count > 0 #outputs the substring and count if > 0
     end
     output
