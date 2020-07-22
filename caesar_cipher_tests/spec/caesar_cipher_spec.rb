5516888bf91dd135d419e4c98b839250fd0176b2
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-22 12:56:33 -0700

    Added RSpec tests

--- caesar_cipher_tests/spec/caesar_cipher_spec.rb
+++ caesar_cipher_tests/spec/caesar_cipher_spec.rb
@@ -1,3 +1,28 @@
-describe Caesar do
-    
+require './lib/caesar_cipher.rb'
+
+describe '#caesar' do
+    it 'works with a single character' do
+    expect(caesar('A', 5)).to eq('F')
+    end
+
+    it 'works with a string' do
+        expect(caesar('Hello', 5)).to eq('Mjqqt')
+    end
+
+    it 'works with puncuation and does not shift' do
+        expect(caesar('Hello!', 5)).to eq('Mjqqt!')
+    end
+
+    it 'works when looping from end of alphabet' do
+        expect(caesar('z', 1)).to eq('a')
+    end
+
+    it 'works with all variations of lower case, upper case, and ignores symbols' do
+        expect(caesar('!DoGs**are**COOL?', 5)).to eq('!ItLx**fwj**HTTQ?')
+    end
+
+    it 'works with negative shift' do
+        expect(caesar('Hello', -5)).to eq('Czggj')
+    end
+
 end
