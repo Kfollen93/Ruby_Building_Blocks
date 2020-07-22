dc1086d047fbc40bf9333bbb209bc62252f3ba8f
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-22 12:55:57 -0700

    Added RSpec tests

--- caesar_cipher_tests/lib/caesar_cipher.rb
+++ caesar_cipher_tests/lib/caesar_cipher.rb
@@ -1,16 +1,16 @@
 
-def caesar(str, shift)
+def caesar(str, number_shift)
     str_arr = str.split('')
     str_arr.map { |char| char.ord }.map { |ord|
         if ord < 65 || ord > 122
             ord.chr
+        elsif ord >= 65 && ord < 97
+            ((ord - 65 + number_shift) % 26 + 65).chr
         else
-            ord > 65 && ord < 97 ?
-            ((ord - 65 + shift)%26 +65).chr :
-            ((ord - 97 + shift)%26 +97).chr    
+            ((ord - 97 + number_shift) % 26 + 97).chr    
         end
 
     }.join()
 end
 
-puts caesar('Sup mate!', 29)
+# puts caesar('Hello', 5)
